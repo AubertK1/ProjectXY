595546b2061adfca68ce97994df7d95f306010f5
jt8500 <jt8500@students.inghamisd.org>
2023-01-04 13:03:57 -0500

    Main menu screen begun

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -19,10 +19,12 @@ public class Main extends ApplicationAdapter {
 	static SpriteBatch batch;
 	//Houses all the groups so that they can all be drawn together
 	static Stage stage;
-	//Houses all the actors so that they can be setup on the screen together and drawn together
-	static Group group;
 	//a spritesheet of all the actors (an actor is a button, textfield, label, etc.)
 	static Skin skin;
+	//main menu screen
+	MainMenu menu;
+	//detects whether the game is currently being played or not
+	static boolean isGameRunning = false;
 
 	/**
 	 * This class sets up the screen. It's only called ONCE (when the game is loaded)
@@ -32,9 +34,13 @@ public class Main extends ApplicationAdapter {
 		//initializing everything
 		batch = new SpriteBatch();
 		stage = new Stage();
-		group = new Group();
 		skin = new Skin(Gdx.files.internal("assets\\skin\\uiskin.json"));
+		//setting main menu screen
+		menu = new MainMenu();
+/*
 
+		//Houses all the actors so that they can be setup on the screen together and drawn together
+		Group group = new Group();
 		//this is generally how to use actors
 		//first make the group and set its position on the screen
 		group.setPosition(Gdx.graphics.getWidth()/2f, 200);
@@ -42,12 +48,9 @@ public class Main extends ApplicationAdapter {
 		Label tempLabel = new Label("This is an actor", skin);
 		//then set its size/position (the position is relative to the position of the group)
 		tempLabel.setPosition(0, 200);
-		//example 2:
-		TextField tempTF = new TextField("So is this...", skin);
-		tempTF.setPosition(0, tempLabel.getY()-tempTF.getHeight());
 		//buttons are usually final, so you can reference it in a listener
 		final TextButton tempButton2 = new TextButton("so is the button below this (CLICK IT)",skin);
-		tempButton2.setPosition(0, tempTF.getY()-tempButton2.getHeight());
+		tempButton2.setPosition(0, tempLabel.getY()-tempButton2.getHeight());
 		final Button tempButton = new Button(skin);
 		//listeners tell the button what to do after you click it
 		tempButton.addListener(new ClickListener(){
@@ -57,22 +60,16 @@ public class Main extends ApplicationAdapter {
 		});
 		tempButton.setSize(150, 30);
 		tempButton.setPosition(0, tempButton2.getY()-tempButton.getHeight());
-		//these kinda suck so I don't use them much
-		final SelectBox<String> tempSB = new SelectBox(skin);
-		tempSB.setSize(300, 30);
-		tempSB.setPosition(0, tempButton.getY()-tempSB.getHeight());
-		tempSB.setItems("Click Here","Anyway you get the point", "these are gonna be used in", "the main menu screen mostly", "but they're useful to know for this");
 
 		//after you make an actor, add it to the group
 		group.addActor(tempLabel);
-		group.addActor(tempTF);
 		group.addActor(tempButton2);
 		group.addActor(tempButton);
-		group.addActor(tempSB);
 
 		//then add the group to the stage and that's it
 		stage.addActor(group);
 
+*/
 		//this is so the user can click on the actors
 		Gdx.input.setInputProcessor(stage);
 	}
@@ -86,8 +83,14 @@ public class Main extends ApplicationAdapter {
 		ScreenUtils.clear(new Color(0x3fc9e8ff));
 		batch.begin();
 		batch.end();
+
+		//if game is not being played, show main menu screen
+		if (isGameRunning == false){
+			menu.draw();
+		}
 		//draws the actors on the screen
 		stage.draw();
+
 	}
 
 	/**
---
+++ core/src/com/mygdx/game/MainMenu.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+public class MainMenu {
+    Skin skin = Main.skin;
+    Stage stage = Main.stage;
+    // Makes a new group
+    Group myGroup = new Group();
+    public MainMenu() {
+        myGroup.setPosition(0,0);
+
+        String myString = "Click here";
+        final TextButton myTextbutton = new TextButton(myString,skin);
+        myTextbutton.setPosition(800,600);
+        myTextbutton.setSize(400,400);
+        myTextbutton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                myTextbutton.setText("click");
+            }
+        });
+
+        myGroup.addActor(myTextbutton);
+        stage.addActor(myGroup);
+    }
+    public void draw(){
+        stage.addActor(myGroup);
+    }
+    public void stopDrawing(){
+        myGroup.addAction(Actions.removeActor());
+    }
+}
