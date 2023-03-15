652d33bfbc4cc295b7ec7acc5c662f9c16ea0044
jt8500 <jt8500@students.inghamisd.org>
2023-03-15 13:59:20 -0400

    Update MainMenu.java

--- core/src/com/mygdx/game/OI/MainMenu.java
+++ core/src/com/mygdx/game/OI/MainMenu.java
@@ -8,57 +8,29 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.game.Main;
 
-public class MainMenu {
-    Skin skin = Main.skin;
-    Stage stage = Main.stage;
-    // Makes a new group
-    Group myGroup = new Group();
+public class MainMenu extends MenuScreen {
+
     public MainMenu() {
-        myGroup.setPosition(0,0);
 
-        String myString = "PLAY";
-        final TextButton myTextbutton = new TextButton(myString,skin);
-        myTextbutton.setPosition(25,550);
-        myTextbutton.setSize(200,200);
-        myTextbutton.addListener(new ChangeListener() {
+        final TextButton play = createButton("PLAY",25,550,200,200);
+        play.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                myTextbutton.setText("click");
-                Main.isMatchRunning = true;
-            }
-        });
-        String aString = "SELECT CHARACTER";
-        final TextButton aTextbutton = new TextButton(aString,skin);
-        aTextbutton.setPosition(25,300);
-        aTextbutton.setSize(200,200);
-        aTextbutton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                aTextbutton.setText("click");
+                play.setText("click");
                 Main.isMatchRunning = true;
             }
         });
 
-        String bString = "Controls";
-        final TextButton bTextbutton = new TextButton(myString,skin);
-        myTextbutton.setPosition(25,100);
-        myTextbutton.setSize(200,200);
-        bTextbutton.addListener(new ChangeListener() {
+        final TextButton controls = createButton("CONTROLS",25,325,200,200);
+
+        controls.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                bTextbutton.setText("click");
-                Main.isMatchRunning = true;
+                controls.setText("click");
+                Main.isMatchRunning = false;
             }
         });
-        myGroup.addActor(myTextbutton);
-        myGroup.addActor(aTextbutton);
 
-    }
-    public void startDrawing(){
-        stage.addActor(myGroup);
-    }
-    public void stopDrawing(){
-        myGroup.remove();
     }
 
 
