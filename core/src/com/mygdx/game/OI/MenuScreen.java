b37c60dc0e4da77ed7735625c0111bc4acbbe0f3
jt8500 <jt8500@students.inghamisd.org>
2023-03-17 13:39:50 -0400

    Update MenuScreen.java

--- core/src/com/mygdx/game/OI/MenuScreen.java
+++ core/src/com/mygdx/game/OI/MenuScreen.java
@@ -8,28 +8,20 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.game.Main;
 
-public class MenuScreen {
-    Skin skin = Main.skin;
-    Stage stage = Main.stage;
-    // Makes a new group
-    Group myGroup = new Group();
+public class MenuScreen extends Screen {
+
+
 
     public MenuScreen() {
-        myGroup.setPosition(0,0);
+        UI.setPosition(0,0);
     }
 
-    public void startDrawing(){
-        stage.addActor(myGroup);
-    }
-    public void stopDrawing(){
-        myGroup.remove();
-    }
 
     public TextButton createButton(String buttonText,int x, int y, int width, int height){
         final TextButton aTextbutton = new TextButton(buttonText,skin);
         aTextbutton.setPosition(x,y);
         aTextbutton.setSize(width,height);
-        myGroup.addActor(aTextbutton);
+        UI.addActor(aTextbutton);
         return aTextbutton;
     }
 }
