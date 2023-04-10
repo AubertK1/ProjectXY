25e4fbbacbe1fd47e65b77d2de236630b21852b5
jt8500 <jt8500@students.inghamisd.org>
2023-04-10 12:55:41 -0400

    Update CharacterSelectScreen.java

--- core/src/com/mygdx/game/OI/CharacterSelectScreen.java
+++ core/src/com/mygdx/game/OI/CharacterSelectScreen.java
@@ -1,4 +1,25 @@
 package com.mygdx.game.OI;
 
-public class CharacterSelectScreen {
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.mygdx.game.Main;
+
+public class CharacterSelectScreen extends MenuScreen{
+
+    @Override
+    public Image characterSelect(filePath, x, y, width, height);
+
+    final ImageButton play = createImageButton("textures/playTemp.PNG",25,550,200,200);
+        play.addListener(new ChangeListener() {
+        @Override
+        public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+            Main.changeScreen("GameScreen");
+        }
+    });
+
+
+
+
+    }
 }
