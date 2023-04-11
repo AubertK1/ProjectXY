4d1e28146d6a6703071d25acd79ed7de7dd6a76f
jt8500 <jt8500@students.inghamisd.org>
2023-04-11 13:20:41 -0400

    Update CharacterSelectScreen.java

--- core/src/com/mygdx/game/OI/CharacterSelectScreen.java
+++ core/src/com/mygdx/game/OI/CharacterSelectScreen.java
@@ -12,7 +12,13 @@ public class CharacterSelectScreen extends MenuScreen{
 
     public Texture characterSelectImage = new Texture("textures/CHOOSE_YOUR_CHARACTER.png");
 
+    public Texture vampireImage = new Texture("textures/Vampire_Bot_48x.png");
+    public Texture violentCyborgImage = new Texture("textures/Violent_Cyborg_48x.png");
+    public Texture securityRobotImage = new Texture("textures/Security_Robot_48x.png");
+
     public CharacterSelectScreen() {
+        background = new Texture("assets\\textures\\Night_Time_Background.png");
+
         final ImageButton fight = createImageButton("textures/FIGHT.PNG", 25, 550, 200, 200);
         fight.addListener(new ChangeListener() {
             @Override
@@ -24,8 +30,11 @@ public class CharacterSelectScreen extends MenuScreen{
 
 
     public void render(SpriteBatch batch){
-       // batch.draw(background,0, 0,2000,1000);
-        batch.draw(characterSelectImage,700, 900, 400, 200);
+        batch.draw(background,0, 0,2000,1000);
+        batch.draw(characterSelectImage,750, 800, 400, 200);
+        batch.draw(vampireImage,100,400,400,300);
+        batch.draw(violentCyborgImage,300,400,300,300);
+        batch.draw(securityRobotImage,500,400,300,300);
     }
 
     }
