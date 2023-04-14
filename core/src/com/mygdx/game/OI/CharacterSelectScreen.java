a91e110e9a399f979f4db587884dd53da2e19815
jt8500 <jt8500@students.inghamisd.org>
2023-04-14 12:05:59 -0400

    Update CharacterSelectScreen.java

--- core/src/com/mygdx/game/OI/CharacterSelectScreen.java
+++ core/src/com/mygdx/game/OI/CharacterSelectScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.game.Main;
 
@@ -12,9 +13,9 @@ public class CharacterSelectScreen extends MenuScreen{
 
     public Texture characterSelectImage = new Texture("textures/CHOOSE_YOUR_CHARACTER.png");
 
-    public Texture vampireImage = new Texture("textures/Vampire_Bot_48x.png");
-    public Texture violentCyborgImage = new Texture("textures/Violent_Cyborg_48x.png");
-    public Texture securityRobotImage = new Texture("textures/Security_Robot_48x.png");
+    public Texture vampireImage = new Texture("textures/Vampire_Bot/Vampire_Bot_48x.png");
+    public Texture violentCyborgImage = new Texture("textures/Violet_Cyborg/Violet_Cyborg_48x.png");
+    public Texture securityRobotImage = new Texture("textures/Security_Robot/Security_Robot_48x.png");
 
     public CharacterSelectScreen() {
         background = new Texture("assets\\textures\\Night_Time_Background.png");
@@ -26,15 +27,22 @@ public class CharacterSelectScreen extends MenuScreen{
                 Main.changeScreen("GameScreen");
             }
         });
+        final TextButton play = createTextButton("BACK",25,200,200,200);
+        play.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Main.changeScreen("MainMenu");
+            }
+        });
     }
 
 
     public void render(SpriteBatch batch){
         batch.draw(background,0, 0,2000,1000);
         batch.draw(characterSelectImage,750, 800, 400, 200);
-        batch.draw(vampireImage,100,400,400,300);
-        batch.draw(violentCyborgImage,300,400,300,300);
-        batch.draw(securityRobotImage,500,400,300,300);
+        batch.draw(vampireImage,250,400,400,300);
+        batch.draw(violentCyborgImage,750,400,300,300);
+        batch.draw(securityRobotImage,1250,400,300,300);
     }
 
     }
