9e81ca4fb6a50f4c22b1b69a88b9d0ce6ed9357a
jt8500 <jt8500@students.inghamisd.org>
2023-04-17 13:56:32 -0400

    Update CharacterSelectScreen.java

--- core/src/com/mygdx/game/OI/CharacterSelectScreen.java
+++ core/src/com/mygdx/game/OI/CharacterSelectScreen.java
@@ -3,10 +3,13 @@ package com.mygdx.game.OI;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.Characters.Cyborg;
+import com.mygdx.game.GameScreen;
 import com.mygdx.game.Main;
 
 public class CharacterSelectScreen extends MenuScreen{
@@ -15,17 +18,8 @@ public class CharacterSelectScreen extends MenuScreen{
     public Texture characterSelectImage = new Texture("textures/CHOOSE_YOUR_CHARACTER.png");
 
     public ImageButton vampireImage = createImageButton("textures/Vampire_Bot/Vampire_Bot_48x.png",250,400,400,300);
-    vampireImage.addlistener(){
-
-    }
     public ImageButton violetCyborgImage = createImageButton("textures/Violet_Cyborg/Violet_Cyborg_48x.png",750,400,300,300);
-    violetCyborgImage.addlistener(){
-
-    }
     public ImageButton securityRobotImage = createImageButton("textures/Security_Robot/Security_Robot_48x.png",1250,400,300,300);
-    securityRobotImage.addlistener(){
-
-    }
     public CharacterSelectScreen() {
         background = new Texture("assets\\textures\\Night_Time_Background.png");
 
@@ -41,6 +35,22 @@ public class CharacterSelectScreen extends MenuScreen{
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 Main.changeScreen("MainMenu");
+            }
+        });
+
+        vampireImage.addListener(new ClickListener(){
+            public void clicked (InputEvent event, float x, float y) {
+                GameScreen.setPlayers(1,"Vampire");
+            }
+        });
+        violetCyborgImage.addListener(new ClickListener(){
+            public void clicked (InputEvent event, float x, float y) {
+                GameScreen.setPlayers(1,"Cyborg");
+            }
+        });
+        securityRobotImage.addListener(new ClickListener(){
+            public void clicked (InputEvent event, float x, float y) {
+                GameScreen.setPlayers(1,"Robot");
             }
         });
     }
