5ddd1d00456e96245b162590309367a54738aa24
jt8500 <jt8500@students.inghamisd.org>
2023-04-19 12:48:19 -0400

    Update MainMenu.java

--- core/src/com/mygdx/game/OI/MainMenu.java
+++ core/src/com/mygdx/game/OI/MainMenu.java
@@ -17,7 +17,7 @@ public class MainMenu extends MenuScreen {
 
         background = new Texture("assets\\textures\\Title_Screen.png");
 
-        final ImageButton play = createImageButton("textures/Start_Button.png",25,550,200,200);
+        final ImageButton play = createImageButton("textures/Start_Button.png",750,550,500,500);
         play.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
