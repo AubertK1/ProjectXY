0d6e0c353e8a46c73a98268f76738c627f58866f
AubertK1 <aubertjkamdem@gmail.com>
2023-04-24 13:22:41 -0400

    ProjectilePool class rework

--- core/src/com/mygdx/game/Projectiles/ProjectilePool.java
+++ core/src/com/mygdx/game/Projectiles/ProjectilePool.java
@@ -2,75 +2,54 @@ package com.mygdx.game.Projectiles;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.LinkedList;
+
 public class ProjectilePool {
-    private Projectile[] busy, idle;
-    private int nextBusyIndex = 0;
-    private int nextIdleIndex = 20;
+    private LinkedList<Projectile> busy, idle;
 
     public ProjectilePool(){
-        busy = new Projectile[20];
-        idle = new Projectile[20];
-
-        for (int i = 0; i < idle.length; i++) {
-            idle[i] = new Projectile();
-        }
+        busy = new LinkedList<>();
+        idle = new LinkedList<>();
     }
 
     public void update(){
         int expirationTime = 300;
-        for (int i = 0; i < nextBusyIndex; i++) {
-            busy[i].activeTime++;
-            if(busy[i].activeTime > expirationTime) free(busy[i]);
+        for (int i = 0; i < busy.size(); i++) {
+            busy.get(i).activeTime++;
+            if(busy.get(i).activeTime > expirationTime) free(busy.get(i));
         }
-
     }
-    public Projectile grab(){
-        for (int i = idle.length - 1; i >= 0; i--) {
-            if(idle[i] != null){
-                Projectile p = idle[i];
-                busy[nextBusyIndex] = p;
-                idle[i] = null;
-
-                nextBusyIndex++;
-                nextIdleIndex--;
+    public Projectile grab(Class projectileType){
+        for (int i = idle.size() - 1; i >= 0; i--) {
+            if (idle.get(i).getClass() == projectileType) {
+                Projectile p = idle.get(i);
+                busy.add(p);
+                idle.remove(p);
 
                 return p;
             }
         }
+        try {
+            //if there aren't any already existing projectiles of the Type
+            Object p = projectileType.newInstance();
+            busy.add((Projectile) p);
+
+            return (Projectile) p;
+        } catch (InstantiationException | IllegalAccessException ie) {
+            ie.printStackTrace();
+        }
+
         return null;
     }
     public void free(Projectile p){
-        int removedIndex = 19;
-        for (int i = 0; i < busy.length; i++) { //looking for the projectile
-            if(busy[i] == p){
-                Projectile p2 = busy[i];
-                p2.wipe();
-                idle[nextIdleIndex] = p2;
-
-                busy[i] = null;
-
-                nextBusyIndex--;
-                nextIdleIndex++;
-                removedIndex = i;
-
-                break;
-            }
-        }
-        Projectile[] b2 = new Projectile[busy.length];
-        for (int i = 0; i < nextBusyIndex + 1; i++) { //getting rid of any gaps in the non-null indexes in busy[]
-            if(i < removedIndex){
-                b2[i] = busy[i];
-            }
-            else if(i > removedIndex){
-                b2[i - 1] = busy[i];
-            }
-        }
-        busy = b2;
+        busy.remove(p);
+        p.wipe();
+        idle.add(p);
     }
 
     public void renderProjectiles(SpriteBatch batch){
-        for (int i = 0; i < nextBusyIndex; i++) {
-            busy[i].render(batch);
+        for (Projectile p : busy) {
+            p.render(batch);
         }
     }
 }
