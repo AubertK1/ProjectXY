e17986f9d269ae7e28200fdb4dc65081d403e3cf
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2023-05-04 09:59:29 -0400

    Started using constants

--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -4,7 +4,10 @@ public class Constants {
     
     // All constants unique to the Cyborg
     public class CyborgConstants {
-
+        public static final int kHurtBoxXOffSet = 13; // (13, 0, 21, 38);
+        public static final int kHurtBoxYOffSet = 0;
+        public static final int kHurtBoxWidth = 21;
+        public static final int kHurtBoxHeight = 38;
     }
 
     // All constants unique to the Robot
